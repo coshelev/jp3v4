35e2e12fbed1e4046f769319a624663fc58c9866
coshelev <coshelev@gmail.com>
2022-07-15 14:33:22 +0300

    df

--- src/main/java/com/mycompany/a/JettyServer.java
+++ src/main/java/com/mycompany/a/JettyServer.java
@@ -1,7 +1,16 @@
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import org.eclipse.jetty.servlet.FilterHolder;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+
 
 public class JettyServer {
 
@@ -10,10 +19,15 @@ public class JettyServer {
 	public void start() throws Exception {
 		System.out.print("Starting jetty...");
 
-		Server server = new Server(8680);
+	Server server = new Server(8680);
 
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+	ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
+
+	FilterHolder filterHolder = new FilterHolder(org.eclipse.jetty.servlets.CrossOriginFilter.class);
+ 	filterHolder.setInitParameter("allowedOrigins", "*");
+	context.addFilter(filterHolder, "/*", null);
+
         server.setHandler(context);
  
         context.addServlet(new ServletHolder(new HelloServlet()),"/*");
@@ -26,7 +40,7 @@ public class JettyServer {
 	// this servlet create web-page with script to create sse-connection to http-server to url "/ssevent/"
 	context.addServlet(new ServletHolder(new Servlet2("From Servlet 2, creating sse-connetion to /ssevent/ url  ")),"/en/*");
 
-	////////context.addServlet(new ServletHolder(new SSEventSourceServlet()),"/ssevent/*"); //=>
+	//// context.addServlet(new ServletHolder(new SSEventSourceServlet()),"/ssevent/*"); //=>
 	SSEventSourceServlet SSEventSrlt = new SSEventSourceServlet();
 
 	//this servlet create sse-stream for /en/ web-page
@@ -41,6 +55,7 @@ public class JettyServer {
             server.join();
         	} catch (Exception e) {           
             	e.printStackTrace();
+		System.out.println("exception on server.start()");
         };
    	}
 }
